b1775bb398797c3b76723330d9de1289a61c8d98
rapkat10 <aminrapkat@gmail.com>
2020-04-14 18:36:38 -0600

    add road

---
+++ src/road.js
@@ -0,0 +1,41 @@
+
+
+export default class Road {
+
+  constructor(game) {
+    this.game = game;
+    this.ctx = this.game.ctx;
+    this.yOffSet = -150;
+    this.road = new Image;
+    this.road.src = "../dist/assets/images/backgroundRoad.png";
+    this.speed = 0;
+
+  }
+
+  speedUp() {
+    if (this.speed >= 5) {
+      this.speed = 5;
+    } else {
+      this.speed += 0.5;
+    }
+  }
+
+  slowDown() {
+    if(this.speed <= 0) {
+      this.speed = 0;
+    }else {
+      this.speed -= 5;
+    }
+  }
+
+  update() {
+    if (this.yOffSet >= 0) this.yOffSet = -150;
+
+    this.ctx.drawImage(this.road, 0, this.yOffSet);
+    this.ctx.drawImage(this.road, 0, this.yOffSet + 150);
+    this.ctx.drawImage(this.road, 0, this.yOffSet + 300);
+
+    this.yOffSet += this.speed;
+  }
+
+}
