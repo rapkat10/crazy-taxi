74e2dbdc27bf28727a7c0c717721f103103d3fef
rapkat10 <aminrapkat@gmail.com>
2020-04-15 23:44:43 -0600

    add restart game

--- src/game.js
+++ src/game.js
@@ -2,6 +2,7 @@ import Road from "./road";
 import PlayerTaxi from "./player";
 import GameControls from "./game_controls";
 import OtherTaxi from "./otherTaxi";
+import isCollided from "./utilities";
 
 export default class Game {
   
@@ -17,19 +18,48 @@ export default class Game {
 
     this.otherTaxis = [];
     setInterval(() => this.createTaxis(), 3000);
+
+    this.paused = false;
+
   }
 
   createTaxis() {
+    if (this.paused) return;
     let otherTaxi = new OtherTaxi(this);
     this.otherTaxis.push(otherTaxi);
   }
 
+  playAgain(e) {
+    if (e.keyCode !== 13) {
+      return;
+    }
+
+    this.otherTaxis = [];
+    this.road.speed = 0;
+    this.playerTaxi.resetPosition();
+    this.paused = false;
+    // let tryAgain = document.querySelector("");
+    // tryAgain.style.display = "";
+    document.onkeydown = null;
+
+  }
+
   update() {
+    if (this.paused) return;
     this.road.update();
     this.playerTaxi.update();
+
     this.otherTaxis.forEach(taxi => {
       taxi.update();
-    })
+    });
+
+    if (isCollided(this.playerTaxi, this.otherTaxis)) {
+      this.paused = true;
+      // let tryAgain = document.querySelector("");
+      // tryAgain.style.display = "";
+      document.onkeydown = e => this.playAgain(e);
+    }
+
   }
 
 }
