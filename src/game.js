06ab0bd9551a5fbbbfd9b60571ddc55ed7cf4be0
rapkat10 <aminrapkat@gmail.com>
2020-04-16 22:10:15 -0600

    clean code

--- src/game.js
+++ src/game.js
@@ -3,6 +3,7 @@ import PlayerTaxi from "./player";
 import GameControls from "./game_controls";
 import OtherTaxi from "./otherTaxi";
 import isCollided from "./utilities";
+import GameOverPage from "./gameOverPage";
 
 export default class Game {
   
@@ -19,33 +20,34 @@ export default class Game {
     this.otherTaxis = [];
     setInterval(() => this.createTaxis(), 3000);
 
-    this.paused = false;
-
+    this.gameOver = false;
   }
 
   createTaxis() {
-    if (this.paused) return;
+    if (this.gameOver) return;
     let otherTaxi = new OtherTaxi(this);
     this.otherTaxis.push(otherTaxi);
   }
 
   playAgain(e) {
-    if (e.keyCode !== 13) {
-      return;
-    }
-
+    if (e.keyCode !== 13) return;
     this.otherTaxis = [];
     this.road.speed = 0;
     this.playerTaxi.resetPosition();
-    this.paused = false;
-    // let tryAgain = document.querySelector("");
-    // tryAgain.style.display = "";
+    this.gameOver = false;
+    document.querySelector(".playAgain").style.display = "none";
+    document.querySelector(".home").style.display = "none";
     document.onkeydown = null;
-
   }
 
   update() {
-    if (this.paused) return;
+    document.querySelector('.start-btn-home').style.display = "none";
+    document.querySelector('.instructions').style.display = "none";
+    if (this.gameOver) return;
+
+    document.querySelector(".playAgain").style.display = "none";
+    document.querySelector(".home").style.display = "none";
+
     this.road.update();
     this.playerTaxi.update();
 
@@ -54,9 +56,14 @@ export default class Game {
     });
 
     if (isCollided(this.playerTaxi, this.otherTaxis)) {
-      this.paused = true;
-      // let tryAgain = document.querySelector("");
-      // tryAgain.style.display = "";
+      document.getElementById("crash").play();
+      this.gameOver = true;
+      let gameOverPage = new GameOverPage(this.ctx);
+      requestAnimationFrame(gameloop);
+      function gameloop() {
+        gameOverPage.update();
+        requestAnimationFrame(gameloop);
+      }
       document.onkeydown = e => this.playAgain(e);
     }
 
