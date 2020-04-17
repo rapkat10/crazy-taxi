121f69b7aa8ee38f8468a927832822038a441731
rapkat10 <aminrapkat@gmail.com>
2020-04-16 22:11:35 -0600

    clean code

--- src/utilities.js
+++ src/utilities.js
@@ -1,13 +1,11 @@
-
-
 export default function isCollided(playerTaxi, otherTaxis) {
   let result = false;
   if (otherTaxis.length > 0) {
     otherTaxis.forEach(otherTaxi => {
       if (!result) {
         if (otherTaxi.positionX === playerTaxi.positionX) {
-          if (Math.abs(otherTaxi.positionY) > 105) {
-            if (Math.abs(otherTaxi.positionY) < 138) {
+          if (otherTaxi.positionY > 105) {
+            if (otherTaxi.positionY < 138) {
               debugger;
               if (!playerTaxi.jumped) {
                 debugger;
